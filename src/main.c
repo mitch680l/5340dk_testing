19cb0d563bbbc75c6e2119d33749c85eee63fab5
mitch115 <m395l680@ku.edu>
2025-09-29 23:10:52 -0500

    working test for nrf5340 and nrf9151

--- src/main.c
+++ src/main.c
@@ -8,36 +8,70 @@
 #include <stddef.h>
 #include <stdint.h>
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
-static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
-static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(DT_ALIAS(led2), gpios);
-static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(DT_ALIAS(led3), gpios);
-static const struct device *uart_dev = DEVICE_DT_GET(DT_NODELABEL(uart1));
+#include <nrfx_uarte.h>
+#include <hal/nrf_gpio.h>
+
+#define UARTE_INST  1
+#define TX_PIN  6
+#define RX_PIN  7
+
+#define UART_DEVICE_NODE DT_NODELABEL(uart1)
+static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
+
+
 #define SYSID 1 
 #define COMPID MAV_COMP_ID_MISSIONPLANNER
-#define TARGET_SYS  1 //target system ID for MAVLink messages(i.e the autopilot)
-#define TARGET_COMP 1 // target component ID for MAVLink messages(i.e the autopilot)
+#define TARGET_SYS  1 
+#define TARGET_COMP 1
 
 
-static void tx_burst(void) {
-    if (!device_is_ready(uart_dev)) {
-        printk("uart1 not ready\n");
-        return;
-    }
-    for (int i = 0; i < 500; ++i) {
-        uart_poll_out(uart_dev, 0x55);
-    }
+static void uart_cb(const struct device *dev, struct uart_event *evt, void *user_data)
+{
+	switch (evt->type) {
+	
+	case UART_TX_DONE:
+		// do something
+        //printk("UART_TX_DONE\n");
+		break;
+
+	case UART_TX_ABORTED:
+		// do something
+        printk("UART_TX_ABORTED\n");
+		break;
+		
+	case UART_RX_RDY:
+		// do something
+		break;
+
+	case UART_RX_BUF_REQUEST:
+		// do something
+		break;
+
+	case UART_RX_BUF_RELEASED:
+		// do something
+		break;
+		
+	case UART_RX_DISABLED:
+		// do something
+		break;
+
+	case UART_RX_STOPPED:
+		// do something
+		break;
+		
+	default:
+		break;
+	}
 }
 
 void Send(const uint8_t *buf, size_t len)
 {
-    if (!device_is_ready(uart_dev)) {
-        return; /* Optionally assert/log */
+    
+    if (uart_tx(uart_dev, buf, len, SYS_FOREVER_MS) != 0) {
+        printk("uart_tx failed\n");
     }
-    for (size_t i = 0; i < len; ++i) {
-        uart_poll_out(uart_dev, buf[i]);
-    }
-}
+    
+} 
 
 void SendHeartbeat(void) {
 
@@ -56,65 +90,25 @@ void SendHeartbeat(void) {
 
     size_t len = mavlink_msg_to_send_buffer(buf, &msg);
     Send(buf, len);
+    //printk("%d \n", buf);
+    printk("Sending Heartbeat\n");
 }
 
 int main(void)
 {
-    if (!device_is_ready(led.port)) {
-        printk("LED device not ready\n");
-        return 0;
-    }
-
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE) != 0) {
-        printk("Failed to configure LED pin\n");
-        return 0;
-    }
-
-    if (!device_is_ready(led1.port)) {
-        printk("LED device not ready\n");
-        return 0;
-    }
-
-    if (gpio_pin_configure_dt(&led1, GPIO_OUTPUT_INACTIVE) != 0) {
-        printk("Failed to configure LED pin\n");
-        return 0;
-    }
-    if (!device_is_ready(led2.port)) {
-        printk("LED device not ready\n");
-        return 0;
-    }
-
-    if (gpio_pin_configure_dt(&led2, GPIO_OUTPUT_INACTIVE) != 0) {
-        printk("Failed to configure LED pin\n");
-        return 0;
-    }
-
-    if (!device_is_ready(led3.port)) {
-        printk("LED device not ready\n");
-        return 0;
-    }
-
-    if (gpio_pin_configure_dt(&led3, GPIO_OUTPUT_INACTIVE) != 0) {
-        printk("Failed to configure LED pin\n");
-        return 0;
-    }
-
+    
     printk("nRF5340 blinky running\n");
-
+    if (!device_is_ready(uart_dev)) {
+        return;
+    }
+    int err;
+    err = uart_callback_set(uart_dev, uart_cb, NULL);
+    if (err) {
+        return err;
+    }
     while (1) {
-        printk("Toggling Red LED\n");
-        gpio_pin_toggle_dt(&led);
-        k_msleep(500);
-        printk("Toggling Blue LED\n");
-        gpio_pin_toggle_dt(&led1);
-        k_msleep(500);
-        printk("Toggling Green LED\n");
-        gpio_pin_toggle_dt(&led2);
-        k_msleep(500);
-        printk("Toggling Green LED\n");
-        gpio_pin_toggle_dt(&led3);
-        k_msleep(500);
         SendHeartbeat();
-        k_msleep(500);
+        k_msleep(50);
     }
 }
+
